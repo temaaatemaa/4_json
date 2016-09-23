6ca2cabb29e58cf657ec61961e912387a7913ffe
temaaatemaa <temka11@me.com>
2016-09-23 20:03:39 +0300

    completed

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,17 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file_handler:
+    return json.load(file_handler)
 
 
 def pretty_print_json(data):
-    pass
+    pprint.pprint(file, width=70,indent=5)
 
 
 if __name__ == '__main__':
     pass
+
+filepath_from_keyboard=input("Введите путь до файла ")
+file=load_data(filepath_from_keyboard)
+pretty_print_json(file)
